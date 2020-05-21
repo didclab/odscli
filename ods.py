339a96fb466c1c96c67a13f2c58b39bd52b42c3d
Bhakti Jadhav <56514727+JadhavBhakti@users.noreply.github.com>
2020-05-20 23:43:32 -0400

    Changed the commands

--- ods.py
+++ ods.py
@@ -3,25 +3,25 @@
 import argparse
 from endpoint.Dropbox import dropbox
 from endpoint.Gdrive import gdrive
+from endpoint.Box import box
 from endpoint.Endpoint import endpoint
 
 
 def argparser_handler(args):
-    if(args.command=='ls/Dropbox'):
+    if(args.command=='ls'):
         if(args.fileName==None):
-            dbxList(args.credential)
+            list(args.credential)
         else:
-            folderlistDbx(args.credential,args.fileName)
-    elif(args.command=='ls/Gdrive'):
-        gdriveList(args.credential)
-    elif(args.command=='mkdir/Dropbox'):
-        dirDbx(args.credential,args.fileName)
-    elif (args.command == 'ls:f/Dropbox'):
-        folderlistDbx(args.credential,args.fileName)
-    elif (args.command == 'rm/Dropbox'):
-        deleteDbx(args.credential,args.fileName)
-    elif (args.command == 'DbxtoGdrive'):
-        DbxtoGdrive(args.credential1,args.credential2,args.fileName)
+            folderlist(args.credential,args.fileName)
+    elif(args.command=='mkdir'):
+        mkdir(args.credential,args.fileName)
+    elif (args.command == 'rm'):
+        delete(args.credential,args.fileName)
+    elif (args.command == 'download'):
+        download(args.credential,args.fileName)
+    elif (args.command == 'transfer'):
+        transfer(args.credential1,args.credential2,args.fileName)
+
         
 def parse_args():
     parser = argparse.ArgumentParser(description="ODS")
@@ -31,100 +31,123 @@ def parse_args():
     subparser.required = True
     parser.set_defaults(funct=argparser_handler)
     
-    sub_parser_listDbx = subparser.add_parser("ls/Dropbox", help="List DropBox Files and Folders.")
-    sub_parser_listDbx.add_argument('-f', dest='fileName', help='user.  If this argument is not passed it will be requested.')
-    sub_parser_listDbx.add_argument('-cred', dest='credential', help='user.  If this argument is not passed it will be requested.')
+    sub_parser_list = subparser.add_parser("ls", help="List Files and Folders.")
+    sub_parser_list.add_argument("credential")
+    sub_parser_list.add_argument('-f', dest='fileName', help='user.  If this argument is not passed it will be requested.')
+    #sub_parser_listDbx.add_argument('-cred', dest='credential', help='user.  If this argument is not passed it will be requested.')
     
-    
-    sub_parser_mkdirDbx = subparser.add_parser("mkdir/Dropbox", help="Make DropBox Folder.")
-    sub_parser_mkdirDbx.add_argument("fileName")
-    #sub_parser_mkdirDbx.add_argument("credential")
-    
+    sub_parser_mkdir = subparser.add_parser("mkdir", help="Make Folder.")
+    sub_parser_mkdir.add_argument("fileName")
+    sub_parser_mkdir.add_argument("credential")
     
     #parser.add_argument('string', help='Input String', nargs='+', action=dirDbx)
-    sub_parser_mkdirDbx.add_argument('-cred', dest='credential', help='user.  If this argument is not passed it will be requested.')
+    #sub_parser_mkdir.add_argument('-cred', dest='credential', help='user.  If this argument is not passed it will be requested.')
     #sub_parser_mkdirDbx.add_argument('file', metavar="fileName" help='user.  If this argument is not passed it will be requested.')
     
     sub_parser_folderlistDbx = subparser.add_parser("ls:f/Dropbox", help="List Files inside DropBox Folder.")
     sub_parser_folderlistDbx.add_argument("fileName")
-    sub_parser_folderlistDbx.add_argument('-cred', dest='credential', help='user.  If this argument is not passed it will be requested.')
+    sub_parser_folderlistDbx.add_argument("credential")
+    #sub_parser_folderlistDbx.add_argument('-cred', dest='credential', help='user.  If this argument is not passed it will be requested.')
     
     
-    sub_parser_deleteDbx = subparser.add_parser("rm/Dropbox", help="Delete DropBox Files.")
-    sub_parser_deleteDbx.add_argument("fileName")
-    sub_parser_deleteDbx.add_argument('-cred', dest='credential', help='user.  If this argument is not passed it will be requested.')
+    sub_parser_delete = subparser.add_parser("rm", help="Delete Files.")
+    sub_parser_delete.add_argument("fileName")
+    sub_parser_delete.add_argument("credential")
+    #sub_parser_deleteDbx.add_argument('-cred', dest='credential', help='user.  If this argument is not passed it will be requested.')
+    
+    sub_parser_download = subparser.add_parser("download", help="Download Files.")
+    sub_parser_download.add_argument("fileName")
+    sub_parser_download.add_argument("credential")
+    #sub_parser_downloadDbx.add_argument('-cred', dest='credential', help='user.  If this argument is not passed it will be requested.')
     
     
-    sub_parser_transferDbxtoGdrive = subparser.add_parser("DbxtoGdrive", help="Transfer Files.")
-    sub_parser_transferDbxtoGdrive.add_argument('-src', dest='credential1', help='user.  If this argument is not passed it will be requested.')
-    sub_parser_transferDbxtoGdrive.add_argument('-dest', dest='credential2', help='user.  If this argument is not passed it will be requested.')
-    sub_parser_transferDbxtoGdrive.add_argument('-file', dest='fileName', help='user.  If this argument is not passed it will be requested.')
-    
-    sub_parser_listGdrive = subparser.add_parser("ls/Gdrive", help="List DropBox Files and Folders.")
-    sub_parser_listGdrive.add_argument('-cred', dest='credential', help='user.  If this argument is not passed it will be requested.')
+    sub_parser_transfer = subparser.add_parser("transfer", help="Transfer Files.")
+    sub_parser_transfer.add_argument("fileName")
+    sub_parser_transfer.add_argument('-src', dest='credential1', help='user.  If this argument is not passed it will be requested.')
+    sub_parser_transfer.add_argument('-dest', dest='credential2', help='user.  If this argument is not passed it will be requested.')
     
     args = parser.parse_args()
     args.funct(args)
+
+
+def getInstance(endpt):
+    if(endpt=='dropbox'):
+        instance=dropbox()
+    elif(endpt=='gdrive'):
+        instance=gdrive()
+    elif(endpt=='box'):
+        instance=box()
+        
+    return instance
     
-def dbxList(credential):
+def list(credential):
     if not credential:
         credential=input('UUID:')
-
-    instance1=dropbox()
-    instance1.credentialId=credential
-    instance1.list()
+        
+    endpt,credential = credential.split('://', 1)
     
-def dirDbx(credential,fileName):
+    obj=getInstance(endpt)
+    obj.credentialId=credential
+    obj.printlist()
+    
+def mkdir(credential,fileName):
     if not credential:
         credential=input('UUID:')
     if not fileName:
         fileName=input('Enter FileName to be Created:')
     
-    instance2=dropbox()
-    instance2.credentialId=credential
-    instance2.mkdir(fileName)
+    endpt,credential = credential.split('://', 1)
     
-def folderlistDbx(credential,fileName):
+    instance=getInstance(endpt)
+    instance.credentialId=credential
+    instance.mkdir(fileName)
+    
+def folderlist(credential,fileName):
     if not credential:
         credential=input('UUID:')
     if not fileName:
         fileName=input('Enter Folder Name to be listed:')
+        
+    endpt,credential = credential.split('://', 1)
     
-    instance3=dropbox()
-    instance3.credentialId=credential
-    instance3.folderfiles(fileName)
+    instance=getInstance(endpt)
+    instance.credentialId=credential
+    instance.folderfiles(fileName)
     
-def deleteDbx(credential,fileName):
+def delete(credential,fileName):
     if not credential:
         credential=input('UUID:')
-    if not fileName:
-        fileName=input('Enter File to be deleted:')
     
-    instance4=dropbox()
-    instance4.credentialId=credential
-    instance4.delete(fileName)
     
-def DbxtoGdrive(credential,credential1,credential2,fileName):
-    if not credential:
-        credential=input('UUID:')
-    if not credential1:
-        credential1=input('DropBox Account:')
-    if not credential2:
-        credential2=input('GoogleDrive Account:')
-    if not fileName:
-        fileName=input('Enter File to be transferred:')
+    endpt,credential = credential.split('://', 1)
     
-    instance5=dropbox()
-    instance5.credentialId=credential
-    instance5.transfer(credential1,credential2,fileName)
+    instance=getInstance(endpt)
+    instance.credentialId=credential
+    instance.delete(fileName)
     
-def gdriveList(credential):
+def download(credential,fileName):
     if not credential:
         credential=input('UUID:')
 
-    instance6=gdrive()
-    instance6.credentialId=credential
-    instance6.list()
+    endpt,credential = credential.split('://', 1)
+    
+    instance=getInstance(endpt)
+    instance.credentialId=credential
+    instance.download(fileName)
+    
+def transfer(credential,dest,fileName):
+    if not credential:
+        credential=input('Dropbox UUID:')
+    if not dest:
+        dest=input('GoogleDrive UUID:')
+    
+    endpt,credential = credential.split('://', 1)
+    
+    instance=getInstance(endpt)
+    instance.credentialId=credential
+    instance.dest=dest
+    instance.transfer(fileName)
+
     
 def main():
     pass
