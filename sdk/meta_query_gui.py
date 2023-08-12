5ff925acc1fd1c17c44ba6e714ccfce362fa5763
Jacob Goldverg <jgoldverg@gmail.com>
2023-08-12 14:10:08 -0500

    forgot to pass in the outputfile

--- sdk/meta_query_gui.py
+++ sdk/meta_query_gui.py
@@ -62,7 +62,7 @@ class QueryGui:
                 print('Influx has no measurements of jobId: ', job_id)
                 local_retry += 1
             else:
-                self.log.visualize_influx_data(job_data_influx)
+                self.log.visualize_influx_data(job_data_influx, output_file)
 
             if Log.check_if_job_done(job_batch_cdb['status']):
                 print('\n JobId: ', job_id, ' has final status of ', job_batch_cdb['status'])
@@ -85,7 +85,7 @@ class QueryGui:
 
         influx_job_data = self.mq.query_job_id_influx(job_id)
         if len(influx_job_data) > 0:
-            self.log.visualize_influx_data(influx_job_data)
+            self.log.visualize_influx_data(influx_job_data, output_file)
 
         local_retry = 0
         end_monitor = False
@@ -109,7 +109,7 @@ class QueryGui:
                 print('Influx has no measurements of jobId: ', job_id)
                 local_retry += 1
             else:
-                self.log.visualize_influx_data(job_data_influx)
+                self.log.visualize_influx_data(job_data_influx, output_file)
 
             if Log.check_if_job_done(job_batch_cdb['status']):
                 print('\n JobId: ', job_id, ' has final status of ', job_batch_cdb['status'])
@@ -225,7 +225,7 @@ class QueryGui:
             print('JobId: ', job_id)
             print('\tJob size in Megabits: ', job_size)
             print('\tTotal Time for job to complete: ', totalSeconds)
-            print('\tTotal Job throughput: ', job_size / totalSeconds)
+            print('\tTotal Job throughput: ', job_size / totalSeconds, "Mbps")
 
     def transform_start_end_last(self, df):
         if 'startTime' in df.columns:
